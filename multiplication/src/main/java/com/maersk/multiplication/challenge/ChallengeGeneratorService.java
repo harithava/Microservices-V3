b7792afef69d49bccbc0561eeaa80a98852ace8d
Hariharan Thavachelvam <hariharan.thavachelvam@maersk.com>
2023-06-01 15:23:43 +0100

    HH03: Added leaderboard functionality.

--- multiplication/src/main/java/com/maersk/multiplication/challenge/ChallengeGeneratorService.java
+++
@@ -1,9 +0,0 @@
-package com.maersk.multiplication.challenge;
-
-public interface ChallengeGeneratorService {
-
-    /**
-     * @return a randomly generated challenge with factors between 11 and 99
-     */
-    Challenge randomChallenge();
-}
